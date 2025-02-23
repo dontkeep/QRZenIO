f8caa16bd38f3e4433efffe994efff4677e2887a
dontkeep <alsdiaryy@gmail.com>
2025-02-23 19:42:21 +0700

    Added Scanner with ZXing Scanner

--- QRZen/src/main/java/com/al/qrzen/scanner/ZenScanner.kt
+++ QRZen/src/main/java/com/al/qrzen/scanner/ZenScanner.kt
@@ -1,7 +1,15 @@
 package com.al.qrzen.scanner
 
+import android.util.Size
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import zxingcpp.BarcodeReader
@@ -15,4 +23,55 @@ fun ZenScannerScreen(
     val lifecycleOwner = LocalLifecycleOwner.current
     val scanner = BarcodeReader()
 
+    AndroidView(
+        factory = { ctx ->
+            val scannerView = PreviewView(ctx)
+            val cameraProviderFuture = ProcessCameraProvider.getInstance(ctx)
+
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+
+                val preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(scannerView.surfaceProvider)
+                }
+
+                val imageAnalysis = ImageAnalysis.Builder()
+                    .setTargetResolution(Size(1280, 720))
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .build()
+
+                imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(ctx)) {
+                    if (isScanningEnabled) {
+                        val resultText = processImageProxy(it, scanner)
+                        if (resultText.isNotEmpty()) {
+                            onQrCodeScanned(resultText)
+                        }
+                    }
+                    it.close()
+                }
+
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA,
+                        preview,
+                        imageAnalysis
+                    )
+                } catch (exc: Exception) {
+                    exc.printStackTrace()
+                }
+            }, ContextCompat.getMainExecutor(ctx))
+
+            scannerView
+        },
+        modifier = modifier
+    )
+}
+
+fun processImageProxy(image: ImageProxy, scanner: BarcodeReader): String {
+    val result = image.use {
+        scanner.read(it)
+    }
+    return result.joinToString("\n")
 }
