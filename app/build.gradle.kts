511724308aaacc691ca7d4029f9169d1afdc2af1
dontkeep <alsdiaryy@gmail.com>
2025-03-13 16:03:49 +0700

    Updated UI and added Clickable link
    ToDo:
    Make a better ReadMe for each feature (bordered scanner & unbordered scanner)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-//    implementation("com.github.dontkeep:QRZen:v1.0.1")
-    implementation(project(":QRZen"))
+    implementation("com.github.dontkeep:QRZen:v1.1.0")
 }
